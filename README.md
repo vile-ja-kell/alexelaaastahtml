df8577003fb6750ae89bc63d3a4ab054118fdf48
Juho Kalberg <juss@vilenkell.eu>
2025-03-31 20:48:02 +0300

    graafikud

---
+++ README.md
@@ -0,0 +1,18 @@
+# Manual
+
+## Section types
+    `section--content` 
+    `section--top`
+    `section--dark`
+    `waves-animation` – add to every section with waves
+## Articles inside sections
+### Text left image right
+    `image-text-content-block block--image-right`
+### Text right image left
+    `image-text-content-block`
+### Only text
+    `content-block`
+### Cards
+    `cards`
+
+
--- main.js
+++ main.js
@@ -19,51 +19,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const menuToggle = document.querySelector('.menu-toggle');
     const menuContainer = document.querySelector('.menu__container');
-    const menuLinks = document.querySelectorAll('.menu__link');
+    const menuButtons = document.querySelectorAll('.menu__button');
 
     menuToggle.addEventListener('click', () => {
         menuContainer.classList.toggle('is-open');
         menuToggle.classList.toggle('is-open');
     });
 
-    for (const link of menuLinks) {
-        link.addEventListener('click', () => {
-            menuContainer.classList.remove('is-open');
-            menuToggle.classList.remove('is-open');
-        });
-    }
-
-    const menuButtons = document.querySelectorAll('.menu__button');
-
     for (const button of menuButtons) {
         button.addEventListener('click', () => {
-            const isExpanded = button.getAttribute('aria-expanded') === 'true';
-            button.setAttribute('aria-expanded', !isExpanded);
-            
-            // Update chevron icon
-            const chevronImg = button.querySelector('.menu__chevron img');
-            chevronImg.src = !isExpanded ? 'img/icons/chevron-up.svg' : 'img/icons/chevron-down.svg';
-            
-            // Toggle the menu list visibility with smooth transition
-            const menuSection = button.closest('.menu__section');
-            const menuList = menuSection.querySelector('.menu__list');
-            
-            if (menuList) {
-                menuList.classList.toggle('is-open');
-            }
-
-            // Close other open menus
-            const otherOpenMenus = document.querySelectorAll('.menu__list.is-open');
-            for (const openMenu of otherOpenMenus) {
-                if (openMenu !== menuList) {
-                    openMenu.classList.remove('is-open');
-                    const parentSection = openMenu.closest('.menu__section');
-                    const parentButton = parentSection.querySelector('.menu__button');
-                    parentButton.setAttribute('aria-expanded', 'false');
-                    const parentChevron = parentButton.querySelector('.menu__chevron img');
-                    parentChevron.src = 'img/icons/chevron-down.svg';
-                }
-            }
+            menuContainer.classList.remove('is-open');
+            menuToggle.classList.remove('is-open');
         });
     }
 });
